bb50a6d3f1302d26d5ccc437dbfb3a0b50786aac
leonardo <leooferreira07@gmail.com>
2021-10-23 13:41:23 -0300

    iniciando nosso javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+const linksSocialMedia = {
+    github:'leooferreira',
+    youtube:'rocketseat',
+    facebook:'leo.ferreira.73157',
+    instagram:'_ferreiraleoo',
+    twitter:'rocketseat',
+}
+
+function changeSocialMediaLinks() {
+    for(let li of socialLinks.children) {
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    }
+}
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url)
+    .then(response => {response.json()})
+    .then(data => {userName.textContent = data.name})
+}
+
+getGithubProfileInfos()
+
+
