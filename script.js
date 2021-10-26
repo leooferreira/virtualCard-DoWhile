b275cdb451ac50dbc171ec4e535e4678843c9ac5
leonardo <leooferreira07@gmail.com>
2021-10-25 21:42:36 -0300

    modificando as infos do card com jS

--- script.js
+++ script.js
@@ -19,9 +19,15 @@ function getGithubProfileInfos() {
     const url = `https://api.github.com/users/${linksSocialMedia.github}`
 
     fetch(url)
-    .then(response => {response.json()})
-    .then(data => {userName.textContent = data.name})
-}
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name
+            userBio.textContent = data.bio
+            userLink.href = data.html_url
+            userLogin.textContent = data.login
+        })
+
+ }
 
 getGithubProfileInfos()
 
